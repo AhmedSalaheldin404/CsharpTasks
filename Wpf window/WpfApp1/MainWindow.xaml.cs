b3245ebb51d99652b5d953e484b52e256ec29480
Ahmed Salah El-Din <salaheldinahmed2024@gmail.com>
2024-08-26 04:35:06 +0300

    Add files via upload

---
+++ Wpf window/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            MessageBox.Show("Password button clicked!");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+          
+            MessageBox.Show("Submit button clicked!");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+           
+            MessageBox.Show("Login button clicked!");
+        }
+    }
+}
